6ef3ac80264f81a999bfa17dd8e4080bb87a363f
Evan Fleet <evfleet@gmail.com>
2017-09-13 19:32:19 -0400

    Additional methods to support verify/reset

--- src/models/user.js
+++ src/models/user.js
@@ -50,6 +50,35 @@ UserSchema.pre('save', async function(next) {
 UserSchema.methods = {
   async comparePassword(password) {
     return bcrypt.compare(password, this.password);
+  },
+
+  emailVerification(token) {
+    if (token == this.verificationToken) {
+      this.verified = true;
+      this.verificationToken = null;
+      this.save();
+      return true;
+    }
+    return false;
+  },
+
+  requestResetPassword() {
+    this.resetToken = crypto.randomBytes(32).toString('hex');
+    this.resetExpires = new Date(Date.now() + 30 * 60000);
+    this.save();
+    return true;
+  },
+
+  resetPassword(password, token) {
+    if (Date.now() > this.resetExpires || token != this.resetToken) {
+      return false;
+    } else {
+      this.resetToken = null;
+      this.resetExpires = null;
+      this.password = password;
+      this.save();
+      return true;
+    }
   }
 };
 
