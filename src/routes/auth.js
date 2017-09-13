594c114620d21e87f63f9e28dbdd97771b78ede8
Evan Fleet <evfleet@gmail.com>
2017-09-13 19:41:18 -0400

    Reset routes implemented

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,5 +5,7 @@ const router = new Router();
 
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
+router.post('/request-reset', AuthController.requestResetPassword);
+router.post('/reset-password', AuthController.resetPassword);
 
 export default router;
